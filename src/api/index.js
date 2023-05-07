1061c51a6565599316f09a88fe189cf5d9f94e0f
Abdul Waheed <68082996+abdulwaheed76@users.noreply.github.com>
2023-05-07 17:46:13 +0500

    added vercel file

--- src/api/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-exports.user = require("./user");
-exports.story = require("./story");
-exports.vote = require("./vote");
-exports.commnet = require("./comment");
--- src/loaders/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-
-exports.initializeServer = require("./server").initializeServer;
